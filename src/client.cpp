d22e787bf76b7c531451614c06e9328f045023d7
ZakyHermawan <zaky.hermawan9615@gmail.com>
2021-06-23 22:16:03 +0700

    query users

--- src/client.cpp
+++ src/client.cpp
@@ -1,10 +1,26 @@
 #include "include/client.h"
 #include "ui_client.h"
 
+#include <QSqlQuery>
+#include <QSqlDatabase>
+
 Client::Client(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::Client)
 {
+    QSqlQuery query(QSqlDatabase::database("goods_connection"));
+    query.exec("select * from goods");
+
+    int index = 1;
+    while(query.next()) {
+        QString nama_barang = query.value(1).toString();
+        int jumlah = query.value(2).toInt();
+        unsigned long long int harga = query.value(3).toLongLong();
+        QString image_path = query.value(4).toString();
+        qDebug() << "id:" << index << "Nama barang:" << nama_barang << "Jumlah:" << jumlah
+                 << "Harga:" << harga << "Image path:" << image_path;
+        ++index;
+    }
 
     ui->setupUi(this);
 }
