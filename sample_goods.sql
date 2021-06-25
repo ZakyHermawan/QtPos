cc1bd3130bc158d8cbe150debc1dca33ccdb3fb7
ZakyHermawan <zaky.hermawan9615@gmail.com>
2021-06-25 18:42:55 +0700

    Insert sample goods

---
+++ sample_goods.sql
@@ -0,0 +1,3 @@
+INSERT INTO goods VALUES(1, 'entah', 1, 10000, 'images/no_image');
+INSERT INTO goods VALUES(2, 'ngasal', 2, 15000, 'images/no_image');
+INSERT INTO goods VALUES(3, 'haha', 5, 5000, 'images/no_image');
