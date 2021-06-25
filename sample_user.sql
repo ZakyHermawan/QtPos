093f915327e80878265d94906f68dec2545e4f28
ZakyHermawan <zaky.hermawan9615@gmail.com>
2021-06-25 19:03:47 +0700

    Hash password

--- sample_user.sql
+++ sample_user.sql
@@ -1,7 +1,7 @@
-INSERT INTO users VALUES(1, 'admin', 'admin', 'admin');
-INSERT INTO users VALUES(6, 'wakil', '123', 'admin');
-INSERT INTO users VALUES(2, 'pengguna', 'pengguna', 'user');
-INSERT INTO users VALUES(3, 'asdfg', 'qwert', 'user');
-INSERT INTO users VALUES(4, 'hahaha', 'hihihi', 'user');
-INSERT INTO users VALUES(5, 'nama', 'password', 'user');
-INSERT INTO users VALUES(7, 'naruto', 'shippuden', 'user');
+INSERT INTO users VALUES(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'admin');
+INSERT INTO users VALUES(6, 'wakil', '202cb962ac59075b964b07152d234b70', 'admin');
+INSERT INTO users VALUES(2, 'pengguna', '8b097b8a86f9d6a991357d40d3d58634', 'user');
+INSERT INTO users VALUES(3, 'asdfg', 'a384b6463fc216a5f8ecb6670f86456a', 'user');
+INSERT INTO users VALUES(4, 'hahaha', 'c4bb408471eb7727e59e11385b0a8c19', 'user');
+INSERT INTO users VALUES(5, 'nama', '5f4dcc3b5aa765d61d8327deb882cf99', 'user');
+INSERT INTO users VALUES(7, 'naruto', '2e74eee42b9578308386119ea54f0afa', 'user');
